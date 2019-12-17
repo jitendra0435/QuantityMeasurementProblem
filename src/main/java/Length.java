3c905165f9d64df900a5c3195d9b119d7cb12072
jitendraB <jitendrabachhav0435@gmail.com>
2019-12-17 17:47:12 +0530

    Refactored Code to calculate for Feet to Inch And viceversa.

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,5 +1,6 @@
 public class Length {
 
+    private static final double FEET_TO_INCH = 12.0;
     enum Unit {FEET, INCH}
 
     private final Unit unit;
@@ -8,6 +9,19 @@ public class Length {
     public Length(Unit unit, double value) {
         this.unit = unit;
         this.value = value;
+    }
+
+    public boolean compare(Length that) {
+        if (this.unit.equals(unit.FEET) && that.unit.equals(unit.FEET))
+            return Double.compare(this.value, that.value) == 0;
+        if (this.unit.equals(unit.INCH) && that.unit.equals(unit.INCH))
+            return Double.compare(this.value, that.value) == 0;
+        if (this.unit.equals(unit.FEET) && that.unit.equals(unit.INCH))
+            return Double.compare(this.value * FEET_TO_INCH, that.value) == 0;
+        if (this.unit.equals(unit.INCH) && that.unit.equals(unit.FEET)) {
+            return Double.compare(this.value, that.value * Math.round(FEET_TO_INCH)) == 0;
+        }
+        return false;
     }
 
     @Override
