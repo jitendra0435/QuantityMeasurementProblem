441422b2fa00a61517dcaceb26e114299e2f05e4
jitendraB <jitendrabachhav0435@gmail.com>
2019-12-17 13:53:28 +0530

    Added Code and Test to check Equality of two Inches Variables.

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -4,9 +4,40 @@ import org.junit.Test;
 public class QuantityMeasurementTest {
 
     @Test
-    public void givenOfAnd0FeetFeet_shouldReturnEqual() {
+    public void givenOf0FeetAnd0Feet_shouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void givenOf0FeetAnd1Feet_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void givenOf_0FeetAnd1Feet_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        boolean equals = feet1.equals(feet2);
+        Assert.assertFalse(equals);
+    }
+
+    @Test
+    public void givenOf0InchAnd0Inch_shouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        boolean equals = inch1.equals(inch2);
+        Assert.assertTrue(equals);
+    }
+
+    @Test
+    public void givenOf0InchAnd1Inch_shouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        boolean equals = inch1.equals(inch2);
+        Assert.assertFalse(equals);
     }
 }
