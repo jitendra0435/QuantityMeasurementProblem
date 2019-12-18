b7cd33426d274a4c67a3f5ab5bcf2436e0dc25cb
jitendraB <jitendrabachhav0435@gmail.com>
2019-12-18 18:55:02 +0530

    Added TestCases for unit Volume.

--- src/test/java/VolumeMeasurementTest.java
+++ src/test/java/VolumeMeasurementTest.java
@@ -1,2 +1,199 @@
+import org.junit.Assert;
+import org.junit.Test;
+import quantitymeasurement.Conversion;
+import quantitymeasurement.Volume;
+
 public class VolumeMeasurementTest {
+    @Test
+    public void givenOf0MLAnd0MlShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 0.0);
+        Volume volume2 = new Volume(Conversion.Unit.ML, 0.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1MLAnd1MlShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.ML, 1.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1MLAnd0MlShouldReturnFalse() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.ML, 0.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertFalse(compare);
+    }
+
+    @Test
+    public void givenOf0LITREAnd0LITREShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.LITRE, 0.0);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 0.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1LITREAnd1LITREShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.LITRE, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 1.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1LITREAnd0LITREShouldReturnFalse() {
+        Volume volume1 = new Volume(Conversion.Unit.LITRE, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 0.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertFalse(compare);
+    }
+
+    @Test
+    public void givenOf0GALLONAnd0GALLONShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.GALLON, 0.0);
+        Volume volume2 = new Volume(Conversion.Unit.GALLON, 0.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1GALLONAnd1GALLONShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.GALLON, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.GALLON, 1.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1GALLONAnd0GALLONShouldReturnFalse() {
+        Volume volume1 = new Volume(Conversion.Unit.GALLON, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.GALLON, 0.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertFalse(compare);
+    }
+
+    @Test
+    public void givenOf1000mlAnd1litShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 1000.0);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 1.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1000mlAnd1GallonShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.GALLON, 0.000264172);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1000litAnd1mlShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.LITRE, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.GALLON, 0.264172);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1000litAnd1GallonShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 1000.0);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 1.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1GALLONAnd1mlShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.GALLON, 0.000264172);
+        Volume volume2 = new Volume(Conversion.Unit.ML, 1.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenOf1000GALLONAnd1litShouldReturnTrue() {
+        Volume volume1 = new Volume(Conversion.Unit.GALLON, 0.264172);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 1.0);
+        boolean compare = volume1.compare(volume2);
+        Assert.assertTrue(compare);
+    }
+    //------------------addition--------
+
+    @Test
+    public void givenAdditionOf10mlAnd10mlShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 10.0);
+        Volume volume2 = new Volume(Conversion.Unit.ML, 10.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(20.0, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf10litAnd10litShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.LITRE, 10.0);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 10.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(20000.0, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf10GallonAnd10GallonShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.GALLON, 10.0);
+        Volume volume2 = new Volume(Conversion.Unit.GALLON, 10.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(75708, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1000mlAnd10litShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 10.0);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 10.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(10010, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1000mlAnd1GallonShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.ML, 10.0);
+        Volume volume2 = new Volume(Conversion.Unit.GALLON, 10.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(37864, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf10litAnd100mlShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.LITRE, 10.0);
+        Volume volume2 = new Volume(Conversion.Unit.ML, 100.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(10100, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf10litAnd1GallonShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.LITRE, 10.0);
+        Volume volume2 = new Volume(Conversion.Unit.GALLON, 1.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(13785, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1GallonAnd10000mlShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.GALLON, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.ML, 10000.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(13785, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1GallonAnd10litShouldReturnEqualsReturn() {
+        Volume volume1 = new Volume(Conversion.Unit.GALLON, 1.0);
+        Volume volume2 = new Volume(Conversion.Unit.LITRE, 10.0);
+        double compare = volume1.addition(volume2);
+        Assert.assertEquals(13785, compare, 0.0);
+    }
 }
--- src/test/java/WeightMeasurementTest.java
+++ src/test/java/WeightMeasurementTest.java
@@ -163,33 +163,157 @@ public class WeightMeasurementTest {
         boolean compare = weight1.compare(weight2);
         Assert.assertTrue(compare);
     }
-    //--------------
 
     @Test
-    public void givenAdditionOf1KGAnd1KGInchEquals2000Gram_shouldReturnTrue() {
-        double weight = Weight.addWeight(1.0, 1.0);
-        Weight weight1 = new Weight(Conversion.Unit.KG, weight);
-        Weight weight2 = new Weight(Conversion.Unit.GRAM, 2000.0);
+    public void givenAdditionOf1TonneAnd1Tonne_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.TONNE, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.TONNE, 1.0);
         boolean compare = weight1.compare(weight2);
         Assert.assertTrue(compare);
     }
 
     @Test
-    public void givenAdditionOf1KGAnd500GramInchEquals1500Gram_shouldReturnTrue() {
-        double weight = Weight.addWeight(1.0, 0.500);
-        Weight weight1 = new Weight(Conversion.Unit.KG, weight);
-        Weight weight2 = new Weight(Conversion.Unit.GRAM, 1500.0);
+    public void givenAdditionOf0TonneAnd0Tonne_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.TONNE, 0.0);
+        Weight weight2 = new Weight(Conversion.Unit.TONNE, 0.0);
         boolean compare = weight1.compare(weight2);
         Assert.assertTrue(compare);
     }
 
     @Test
-    public void givenAdditionOf1KGAnd1QuintalEquals2000Gram_shouldReturnTrue() {
-        double weight = Weight.addWeight(1.0, 10.0);
-        Weight weight1 = new Weight(Conversion.Unit.KG, weight);
-        Weight weight2 = new Weight(Conversion.Unit.GRAM, 11000.0);
+    public void givenAdditionOf1TonneAnd0Tonne_shouldReturnFalse() {
+        Weight weight1 = new Weight(Conversion.Unit.TONNE, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.TONNE, 0.0);
+        boolean compare = weight1.compare(weight2);
+        Assert.assertFalse(compare);
+    }
+
+    @Test
+    public void givenAdditionOf1TonneAnd1Quintal_shouldReturnFalse() {
+        Weight weight1 = new Weight(Conversion.Unit.TONNE, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.QUINTAL, 1.0);
+        boolean compare = weight1.compare(weight2);
+        Assert.assertFalse(compare);
+    }
+
+    @Test
+    public void givenAdditionOf1TonneAnd10Quintal_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.TONNE, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.QUINTAL, 10.0);
         boolean compare = weight1.compare(weight2);
         Assert.assertTrue(compare);
     }
 
+    @Test
+    public void givenAdditionOf1TonneAnd1000KG_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.TONNE, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.KG, 1000.0);
+        boolean compare = weight1.compare(weight2);
+        Assert.assertTrue(compare);
+    }
+
+    @Test
+    public void givenAdditionOf1TonneAnd10000GM_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.TONNE, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.GRAM, 1000000.0);
+        boolean compare = weight1.compare(weight2);
+        Assert.assertTrue(compare);
+    }
+    //--------------addition----------------
+
+    @Test
+    public void givenAdditionOf1GramAnd1GramInchEquals2Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.GRAM, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.GRAM, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(2.0, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1KGAnd9KgEquals10000Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.KG, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.KG, 9.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(10000.0, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1TonneAnd1TonneInchEquals2000000Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.TONNE, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.TONNE, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(2000000.0, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1QuintalAnd1QuintalInchEquals200000Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.QUINTAL, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.QUINTAL, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(200000.0, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1GmAnd1QuintalInchEquals100001Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.GRAM, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.QUINTAL, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(100001, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1GmAnd1KgInchEquals1001Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.GRAM, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.KG, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(1001, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf10000GmAnd1TonneInchEquals1010000Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.GRAM, 10000.0);
+        Weight weight2 = new Weight(Conversion.Unit.TONNE, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(1010000, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1KgAnd1QuintalInchEquals101000Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.KG, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.QUINTAL, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(101000, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1KgAnd1TonneInchEquals1000001Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.GRAM, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.TONNE, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(1000001, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1KgAnd9KgEquals2000Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.KG, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.KG, 1.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(2000, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf10KgAnd1000GramInchEquals1_00001E9Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.KG, 10.0);
+        Weight weight2 = new Weight(Conversion.Unit.TONNE, 1000.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(1.00001E9, compare, 0.0);
+    }
+
+    @Test
+    public void givenAdditionOf1QuintalAnd10TonneInchEquals1_01E7Gram_shouldReturnTrue() {
+        Weight weight1 = new Weight(Conversion.Unit.QUINTAL, 1.0);
+        Weight weight2 = new Weight(Conversion.Unit.TONNE, 10.0);
+        double compare = weight1.addition(weight2);
+        Assert.assertEquals(1.01E7, compare, 0.0);
+    }
 }
